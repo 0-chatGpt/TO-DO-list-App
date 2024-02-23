3f465d19758a0af652735feae9b4e8988ba63b87
Chinemeremun Okereke <goldenEye56.me@gmail.com>
2024-02-23 22:52:35 +0100

    added some assets and resolved minor tasks

--- main.js
+++ main.js
@@ -1,46 +1,134 @@
 "use strict";
 
-/**
- * Build a simple To-do list application.
- * The user of this application should be able to add, delete, and update items in the To-do app.
- * This application should have a simple UI but a nice UX.
- */
+class List {
+  constructor(element, todo = null, checked = null, setDate = null, id = null) {
+    this.element = element;
+    this.todo = todo;
+    this.checked = checked;
+    this.setDate = setDate;
+    this.id = id ? id : Date.now();
+  }
 
-// Document Objects
-const todo = document.getElementById('to-do');
-const dateIcon = document.getElementById('date-icon');
-const dateInput = document.getElementById('date');
-const addBtn = document.getElementById('btn-icon');
-const todoForm = document.getElementById('basic-input');
-const editIcon = document.getElementById('edit');
-const delIcon = document.getElementById('delete');
-const checkPoint = document.querySelector('#check-box');
+  apply() {
+    let item = `  <li>
+    <div class="" id="list">
+    <p class="showDate">Date created - ${this.setDate}</p>
+    <input type="checkbox" name="checkbox" class="check-box${this.id}" id="check-box">
+    <input class="show show${this.id}" type="text" value="${this.todo}"  maxlength = "100"></input>
+    <!-- <img src="./pencil.png" class="icon edit${this.id}" id="edit"> -->
+    <img src="./delete.png" class="icon delete${this.id}" id="delete">
+    </div>
+    </li>`;
+    this.element.insertAdjacentHTML("beforeend", item);
 
+    return this;
+  }
 
-const createTodo = function (){
-    let record = {task: todo.value, setDate:dateInput.value, kill: false};
-    localStorage.setItem('Data', JSON.stringify(record));
-    return true;
+  active() {
+    // const editIcon = document.querySelector(`.edit${this.id}`);
+    const delIcon = document.querySelector(`.delete${this.id}`);
+    const checkPoint = document.querySelector(`.check-box${this.id}`);
+    const disp = document.querySelector(`.show${this.id}`);
+    const id = this.id;
+
+    delIcon.addEventListener("click", function (e) {
+      let input = window.confirm("Do want to delete this to-do ? ");
+      if (input) {
+        let newData = JSON.parse(localStorage.getItem("Data"));
+        newData.forEach((el, i) => {
+          if (el.id === id) {
+            newData.splice(i, 1);
+            localStorage.setItem("Data", JSON.stringify(newData));
+          }
+        });
+        this.parentElement.parentElement.remove();
+      }
+      if (!input) {
+        return;
+      }
+    });
+
+    checkPoint.addEventListener("click", function (e) {
+      if (e.target.checked) {
+        disp.style.textDecoration = "line-through";
+      } else {
+        disp.style.textDecoration = "none";
+      }
+    });
+
+    disp.addEventListener("keydown", (e) => {
+      if (e.keyCode === 13) {
+        todo.blur();
+
+        let newData = JSON.parse(localStorage.getItem("Data"));
+        newData.forEach((el, i) => {
+          if (el.id === id) {
+            el.todo = disp.value;
+            // console.log(newData);
+            localStorage.setItem("Data", JSON.stringify(newData));
+          }
+        });
+      }
+    });
+
+    return this;
+  }
 }
 
-todoForm.addEventListener('submit', function (event){
-    event.preventDefault();
-    createTodo();
+// Document Objects
+const todo = document.querySelector("#to-do");
+const dateInput = document.getElementById("date-input");
+const addBtn = document.getElementById("btn-icon");
+const todoForm = document.getElementById("basic-input");
+const unorderedList = document.querySelector(".list");
 
-    this.reset();
-    console.log(JSON.parse(localStorage.getItem('Data')));
+const createTodo = function () {
+  let record = { task: todo.value, setDate: dateInput.value, kill: false };
+  let listItem = new List(
+    unorderedList,
+    record.task,
+    record.kill,
+    record.setDate
+  );
+  let Data = JSON.parse(localStorage.getItem("Data"));
+  Data.push(listItem);
+  localStorage.setItem("Data", JSON.stringify(Data));
+  listItem.apply();
+  listItem.active();
+};
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  createTodo();
+  this.reset();
 });
 
-// checkPoint.addEventListener('click', function (e){
-//     console.log(e.target.checked);
-// })
+todo.addEventListener("input", (e) => {
+  let max = todo.getAttribute("maxlength");
+  if (todo.value.length >= max) {
+    alert(`value must be less than  ${max} characters`);
+  }
+});
 
-editIcon.addEventListener('click', function(){
-    // Display input as editable
-    // Save and render input back to new edit
-})
+todo.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    todo.blur();
+  }
+});
 
-delIcon.addEventListener('click', function(){
-    // Delete particular list item.
-    // delete from record ? localStorage.
-})
+window.onload = () => {
+  localStorage.getItem("Data")
+    ? console.log("true")
+    : localStorage.setItem("Data", JSON.stringify([]));
+  let initials = JSON.parse(localStorage.getItem("Data"));
+  // console.log(initials);
+  // ? JSON.parse(localStorage.getItem("Data")) : localStorage.setItem('Data', JSON.stringify([])) && [];
+  if (initials.length != 0) {
+    initials.forEach((el) => {
+      new List(unorderedList, el.todo, el.checked, el.setDate, el.id)
+        .apply()
+        .active();
+    });
+  }
+};
